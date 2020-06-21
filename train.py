6f296ffc9720d4861b3829f69994f0210dc13b03
Amin Dada <amin.dada@ruhr-uni-bochum.de>
2020-06-21 17:32:13 +0200

    Create new model without bert preprocessing

--- train.py
+++ train.py
@@ -1,24 +1,19 @@
-import gc
-import os
-import re
-import string
-import operator
-
-import numpy as np
+import tweet_tokenizer
 import pandas as pd
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+import numpy as np
+import string
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Dense, LSTM, Bidirectional, Embedding, Dropout
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+# To run this you need to download the pretrained embeddings: http://nlp.stanford.edu/data/glove.twitter.27B.zip
 
-from wordcloud import STOPWORDS
-from preprocessing import clean
-from collections import defaultdict
-from nn import DisasterDetector
-
-def prepare_dataset():
+def load_dataset():
     df_train = pd.read_csv('train.csv', dtype={'id': np.int16, 'target': np.int8})
     df_test = pd.read_csv('test.csv', dtype={'id': np.int16})
 
@@ -27,193 +22,95 @@ def prepare_dataset():
     print('Test Set Shape = {}'.format(df_test.shape))
     print('Test Set Memory Usage = {:.2f} MB'.format(df_test.memory_usage().sum() / 1024**2))
 
-    # missing keyboards location
-    missing_cols = ['keyword', 'location']
-    '''
-        fig, axes = plt.subplots(ncols=2, figsize=(17, 4), dpi=100)
-
-        sns.barplot(x=df_train[missing_cols].isnull().sum().index, y=df_train[missing_cols].isnull().sum().values, ax=axes[0])
-        sns.barplot(x=df_test[missing_cols].isnull().sum().index, y=df_test[missing_cols].isnull().sum().values, ax=axes[1])
-
-        axes[0].set_ylabel('Missing Value Count', size=15, labelpad=20)
-        axes[0].tick_params(axis='x', labelsize=15)
-        axes[0].tick_params(axis='y', labelsize=15)
-        axes[1].tick_params(axis='x', labelsize=15)
-        axes[1].tick_params(axis='y', labelsize=15)
-
-        axes[0].set_title('Training Set', fontsize=13)
-        axes[1].set_title('Test Set', fontsize=13)
-
-        plt.show()
-    '''
-    for df in [df_train, df_test]:
-        for col in ['keyword', 'location']:
-            df[col] = df[col].fillna(f'no_{col}')
-
-
-    # word_count
-    df_train['word_count'] = df_train['text'].apply(lambda x: len(str(x).split()))
-    df_test['word_count'] = df_test['text'].apply(lambda x: len(str(x).split()))
-
-    # unique_word_count
-    df_train['unique_word_count'] = df_train['text'].apply(lambda x: len(set(str(x).split())))
-    df_test['unique_word_count'] = df_test['text'].apply(lambda x: len(set(str(x).split())))
-
-    # stop_word_count
-    df_train['stop_word_count'] = df_train['text'].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
-    df_test['stop_word_count'] = df_test['text'].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
-
-    # url_count
-    df_train['url_count'] = df_train['text'].apply(lambda x: len([w for w in str(x).lower().split() if 'http' in w or 'https' in w]))
-    df_test['url_count'] = df_test['text'].apply(lambda x: len([w for w in str(x).lower().split() if 'http' in w or 'https' in w]))
-
-    # mean_word_length
-    df_train['mean_word_length'] = df_train['text'].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
-    df_test['mean_word_length'] = df_test['text'].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
-
-    # char_count
-    df_train['char_count'] = df_train['text'].apply(lambda x: len(str(x)))
-    df_test['char_count'] = df_test['text'].apply(lambda x: len(str(x)))
-
-    # punctuation_count
-    df_train['punctuation_count'] = df_train['text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
-    df_test['punctuation_count'] = df_test['text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]))
-
-    # hashtag_count
-    df_train['hashtag_count'] = df_train['text'].apply(lambda x: len([c for c in str(x) if c == '#']))
-    df_test['hashtag_count'] = df_test['text'].apply(lambda x: len([c for c in str(x) if c == '#']))
-
-    # mention_count
-    df_train['mention_count'] = df_train['text'].apply(lambda x: len([c for c in str(x) if c == '@']))
-    df_test['mention_count'] = df_test['text'].apply(lambda x: len([c for c in str(x) if c == '@']))
-
-    #df_train['text_cleaned'] = df_train['text'].apply(lambda s : clean(s))
-    #df_test['text_cleaned'] = df_test['text'].apply(lambda s : clean(s))
-
-    df_train['text_cleaned'] = df_train['text']
-    df_test['text_cleaned'] = df_test['text']
-
-    # **4.2 Text Cleaning**
-    df_train['target_relabeled'] = df_train['target'].copy() 
-
-    df_train.loc[df_train['text'] == 'like for the music video I want some real action shit like burning buildings and police chases not some weak ben winston shit', 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == 'Hellfire is surrounded by desires so be careful and donÛªt let your desires control you! #Afterlife', 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == 'To fight bioterrorism sir.', 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == '.POTUS #StrategicPatience is a strategy for #Genocide; refugees; IDP Internally displaced people; horror; etc. https://t.co/rqWuoy1fm4', 'target_relabeled'] = 1
-    df_train.loc[df_train['text'] == 'CLEARED:incident with injury:I-495  inner loop Exit 31 - MD 97/Georgia Ave Silver Spring', 'target_relabeled'] = 1
-    df_train.loc[df_train['text'] == '#foodscare #offers2go #NestleIndia slips into loss after #Magginoodle #ban unsafe and hazardous for #humanconsumption', 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == 'In #islam saving a person is equal in reward to saving all humans! Islam is the opposite of terrorism!', 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == 'Who is bringing the tornadoes and floods. Who is bringing the climate change. God is after America He is plaguing her\n \n#FARRAKHAN #QUOTE', 'target_relabeled'] = 1
-    df_train.loc[df_train['text'] == 'RT NotExplained: The only known image of infamous hijacker D.B. Cooper. http://t.co/JlzK2HdeTG', 'target_relabeled'] = 1
-    df_train.loc[df_train['text'] == "Mmmmmm I'm burning.... I'm burning buildings I'm building.... Oooooohhhh oooh ooh...", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "wowo--=== 12000 Nigerian refugees repatriated from Cameroon", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "He came to a land which was engulfed in tribal war and turned it into a land of peace i.e. Madinah. #ProphetMuhammad #islam", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "Hellfire! We donÛªt even want to think about it or mention it so letÛªs not do anything that leads to it #islam!", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "The Prophet (peace be upon him) said 'Save yourself from Hellfire even if it is by giving half a date in charity.'", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "Caution: breathing may be hazardous to your health.", 'target_relabeled'] = 1
-    df_train.loc[df_train['text'] == "I Pledge Allegiance To The P.O.P.E. And The Burning Buildings of Epic City. ??????", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "#Allah describes piling up #wealth thinking it would last #forever as the description of the people of #Hellfire in Surah Humaza. #Reflect", 'target_relabeled'] = 0
-    df_train.loc[df_train['text'] == "that horrible sinking feeling when youÛªve been at home on your phone for a while and you realise its been on 3G this whole time", 'target_relabeled'] = 0
-    
     return df_train, df_test
 
-def text_embeddings(df_train, df_test):
-    glove_embeddings, fasttext_embeddings = load_pretrained_embeddings()
 
-    train_glove_oov, train_glove_vocab_coverage, train_glove_text_coverage = check_embeddings_coverage(df_train['text'], glove_embeddings)
-    test_glove_oov, test_glove_vocab_coverage, test_glove_text_coverage = check_embeddings_coverage(df_test['text'], glove_embeddings)
-    print('GloVe Embeddings cover {:.2%} of vocabulary and {:.2%} of text in Training Set'.format(train_glove_vocab_coverage, train_glove_text_coverage))
-    print('GloVe Embeddings cover {:.2%} of vocabulary and {:.2%} of text in Test Set'.format(test_glove_vocab_coverage, test_glove_text_coverage))
-
-    train_fasttext_oov, train_fasttext_vocab_coverage, train_fasttext_text_coverage = check_embeddings_coverage(df_train['text'], fasttext_embeddings)
-    test_fasttext_oov, test_fasttext_vocab_coverage, test_fasttext_text_coverage = check_embeddings_coverage(df_test['text'], fasttext_embeddings)
-    print('FastText Embeddings cover {:.2%} of vocabulary and {:.2%} of text in Training Set'.format(train_fasttext_vocab_coverage, train_fasttext_text_coverage))
-    print('FastText Embeddings cover {:.2%} of vocabulary and {:.2%} of text in Test Set'.format(test_fasttext_vocab_coverage, test_fasttext_text_coverage))
-
-    del glove_embeddings, fasttext_embeddings, train_glove_oov, test_glove_oov, train_fasttext_oov, test_fasttext_oov
-    gc.collect()
-
-## **4. Embeddings and Text Cleaning**
-def load_pretrained_embeddings():
-    glove_embeddings = np.load('../input/glove.840B.300d.pkl', allow_pickle=True)
-    fasttext_embeddings = np.load('../input/crawl-300d-2M.pkl', allow_pickle=True)
-    return glove_embeddings, fasttext_embeddings
-
-def train_model(df_train):
-    clf = DisasterDetector(max_seq_length=128, lr=0.0001, epochs=10, batch_size=32)
-
-    clf.train(df_train)
-    clf.plot_learning_curve()
-
-def build_vocab(X):
-    tweets = X.apply(lambda s: s.split()).values      
-    vocab = {}
+def get_vectorizer(df_train, df_test):
     
-    for tweet in tweets:
-        for word in tweet:
-            try:
-                vocab[word] += 1
-            except KeyError:
-                vocab[word] = 1                
-    return vocab
+    # Vectorizes and pads dataset
+    # Also lowers and strips punctuation
+    vectorizer = TextVectorization(max_tokens=7500, output_sequence_length=200)
+    text_ds = tf.data.Dataset.from_tensor_slices(df_train['text']).batch(32)
+    vectorizer.adapt(text_ds)
 
-def check_embeddings_coverage(X, embeddings):
-    vocab = build_vocab(X)    
+    return vectorizer
+
+
+def get_embedding(vectorizer):
+    voc = vectorizer.get_vocabulary()
+    word_index = dict(zip(voc, range(2, len(voc))))
+
+    # Load pretrained embeddings
+    glove_file = "glove.twitter.27B.50d.txt"
+
+    # Parse embeddings
+    embeddings_index = {}
+    with open(glove_file) as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, "f", sep=" ")
+            embeddings_index[word] = coefs
+
+    # 2 extra tokens for padding and unkown words
+    num_tokens = len(voc) + 2
+    embedding_dim = 50
+    hits = 0
+    misses = 0
+
+    # Prepare embedding matrix
+    embedding_matrix = np.zeros((num_tokens, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word.decode("utf-8"))
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            # This includes the representation for "padding" and "OOV"
+            embedding_matrix[i] = embedding_vector
+            hits += 1
+        else:
+            misses += 1
+    print("Converted %d words (%d misses)" % (hits, misses))
+
+    return Embedding(
+                num_tokens,
+                embedding_dim,
+                embeddings_initializer=keras.initializers.Constant(embedding_matrix),
+                trainable=False,
+            )
+
+def get_model(embedding_layer):
+    inputs = Input(shape=(None,), dtype="int64")
+    # Embed input in a 50d vector
+    x = embedding_layer(inputs)
+    # Add 2 bidirectional LSTMs
+    x = Bidirectional(LSTM(64))(x)
+    x = Dropout(rate = 0.5)(x)
+    # Add a classifier
+    outputs = Dense(1, activation="sigmoid")(x)
+    model = keras.Model(inputs, outputs)
+    model.summary()
+    return model
+
+
+
+if __name__ == "__main__":
+    df_train, df_test = load_dataset()
+
+    # Clean everything included in p.set_options
+    df_train['text'] = df_train.apply(lambda x: tweet_tokenizer.tokenize(x.text), axis = 1)
+    df_test['text'] = df_test.apply(lambda x: tweet_tokenizer.tokenize(x.text), axis = 1)
+
+    vectorizer = get_vectorizer(df_train, df_test)
+    embedding_layer = get_embedding(vectorizer)
+    model = get_model(embedding_layer)
+
+    df_train, df_val = train_test_split(df_train, test_size = 0.2)
+
+    x_train = vectorizer(df_train['text'].to_numpy()[..., np.newaxis]).numpy()
+    x_val = vectorizer(df_val['text'].to_numpy()[..., np.newaxis]).numpy()
+
+    y_train = df_train['target'].to_numpy()
+    y_val = df_val['target'].to_numpy()
     
-    covered = {}
-    oov = {}    
-    n_covered = 0
-    n_oov = 0
-    
-    for word in vocab:
-        try:
-            covered[word] = embeddings[word]
-            n_covered += vocab[word]
-        except:
-            oov[word] = vocab[word]
-            n_oov += vocab[word]
-            
-    vocab_coverage = len(covered) / len(vocab)
-    text_coverage = (n_covered / (n_covered + n_oov))
-    
-    sorted_oov = sorted(oov.items(), key=operator.itemgetter(1))[::-1]
-    return sorted_oov, vocab_coverage, text_coverage
+    model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+    model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_val, y_val))
 
-def generate_ngrams(text, n_gram=1):
-    token = [token for token in text.lower().split(' ') if token != '' if token not in STOPWORDS]
-    ngrams = zip(*[token[i:] for i in range(n_gram)])
-    return [' '.join(ngram) for ngram in ngrams]
-
-def plot_distributions(df_train, df_test):
-    METAFEATURES = ['word_count', 'unique_word_count', 'url_count', 'mean_word_length',
-                'char_count', 'punctuation_count', 'hashtag_count', 'mention_count']
-    DISASTER_TWEETS = df_train['target'] == 1
-
-    fig, axes = plt.subplots(ncols=2, nrows=len(METAFEATURES), figsize=(20, 50), dpi=100)
-
-    for i, feature in enumerate(METAFEATURES):
-        sns.distplot(df_train.loc[~DISASTER_TWEETS][feature], label='Not Disaster', ax=axes[i][0], color='green')
-        sns.distplot(df_train.loc[DISASTER_TWEETS][feature], label='Disaster', ax=axes[i][0], color='red')
-
-        sns.distplot(df_train[feature], label='Training', ax=axes[i][1])
-        sns.distplot(df_test[feature], label='Test', ax=axes[i][1])
-        
-        for j in range(2):
-            axes[i][j].set_xlabel('')
-            axes[i][j].tick_params(axis='x', labelsize=12)
-            axes[i][j].tick_params(axis='y', labelsize=12)
-            axes[i][j].legend()
-        
-        axes[i][0].set_title(f'{feature} Target Distribution in Training Set', fontsize=13)
-        axes[i][1].set_title(f'{feature} Training & Test Set Distribution', fontsize=13)
-
-    plt.show()
-
-def main():
-    df_train, df_test = prepare_dataset()
-    #text_embeddings(df_train, df_test)
-    
-    train_model(df_train)
-
-if __name__ == '__main__':
-    main()
