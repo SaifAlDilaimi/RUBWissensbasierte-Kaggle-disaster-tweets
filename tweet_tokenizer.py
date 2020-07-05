77afa6a75945bc44b66b6181e6bf18b0840739e9
Amin Dada <amin.dada@ruhr-uni-bochum.de>
2020-07-05 11:14:52 +0200

    Improve tokens

--- tweet_tokenizer.py
+++ tweet_tokenizer.py
@@ -41,18 +41,20 @@ def tokenize(text):
     def re_sub(pattern, repl):
         return re.sub(pattern, repl, text, flags=FLAGS)
 
-    text = re_sub(r"https?:\/\/\S+\b|www\.(\w+\.)+\S*", "<url>")
+    text = re_sub(r"https?:\/\/\S+\b|www\.(\w+\.)+\S*", " <url> ")
     text = re_sub(r"/"," / ")
-    text = re_sub(r"@\w+", "<user>")
-    text = re_sub(r"{}{}[)dD]+|[)dD]+{}{}".format(eyes, nose, nose, eyes), "<smile>")
-    text = re_sub(r"{}{}p+".format(eyes, nose), "<lolface>")
-    text = re_sub(r"{}{}\(+|\)+{}{}".format(eyes, nose, nose, eyes), "<sadface>")
-    text = re_sub(r"{}{}[\/|l*]".format(eyes, nose), "<neutralface>")
+    text = re_sub(r"@\w+", " <user> ")
+    text = re_sub(r" {}{}[)D]+ | [)D]+{}{} ".format(eyes, nose, nose, eyes), " <smile> ")
+    text = re_sub(r" {}{}p+ ".format(eyes, nose), " <lolface> ")
+    text = re_sub(r" {}{}\(+ | \)+{}{} ".format(eyes, nose, nose, eyes), " <sadface> ")
+    text = re_sub(r" {}{}[\/|l*] ".format(eyes, nose), " <neutralface> ")
     text = re_sub(r"<3","<heart>")
-    text = re_sub(r"[-+]?[.\d]*[\d]+[:,.\d]*", "<number>")
+    text = re_sub(r"[-+]?[.\d]*[\d]+[:,.\d]*", " <number> ")
     text = re_sub(r"#\S+", hashtag)
-    text = re_sub(r"([!?.]){2,}", r"\1 <repeat>")
-    text = re_sub(r"\b(\S*?)(.)\2{2,}\b", r"\1\2 <elong>")
+    text = re_sub(r"([!?.]){2,}", r"\1 <repeat> ")
+    text = re_sub(r"\b(\S*?)(.)\2{2,}\b", r"\1\2  <elong> ")
+
+    text = re_sub(r' +', ' ') 
 
 
     text = re_sub(r"([A-Z]){2,}", allcaps)
